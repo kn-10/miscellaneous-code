396413715ce437bd0f7aea7ce3d69047a9a5f992
metal <metalheadkrn@gmail.com>
2026-01-10 13:11:05 +0530

    Giving IAM role access to the Prometheus server

--- prometheus/main.tf
+++ prometheus/main.tf
@@ -20,9 +20,10 @@ data "aws_ami" "centos8" {
 }
 
 resource "aws_instance" "prometheus" {
-    ami           = data.aws_ami.centos8.image_id
-    instance_type = "t3.small"
+    ami                    = data.aws_ami.centos8.image_id
+    instance_type          = "t3.small"
     vpc_security_group_ids = ["sg-0a13f9496e8f730c7"]
+    iam_instance_profile   = aws_iam_instance_profile.main.name
     tags = {
         Name = "prometheus-server"
     }
@@ -43,4 +44,46 @@ resource "aws_route53_record" "prometheus-public" {
   ttl     = 30
   records = [aws_instance.prometheus.public_ip]
 }
+
+resource "aws_iam_role" "main" {
+  name               = "prometheus-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  inline_policy {
+    name = "parameter-store"
+
+    policy = jsonencode({
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Sid": "VisualEditor0",
+          "Effect": "Allow",
+          "Action": [
+            "ec2:DescribeInstances",
+            "ec2:DescribeAvailabilityZones"
+          ],
+          "Resource": "*"
+        }
+      ]
+    })
+  }
+}
+
+resource "aws_iam_instance_profile" "main" {
+  name = "prometheus-role"
+  role = aws_iam_role.main.name
+}
+
 
