37f270a994513fbd7e7b98a19dbf67cf30aafe05
metal <metalheadkrn@gmail.com>
2026-01-14 20:09:39 +0530

    Giving IAM role access to the Prometheus server

--- prometheus/main.tf
+++ prometheus/main.tf
@@ -24,7 +24,6 @@ resource "aws_instance" "prometheus" {
     instance_type          = "t3.small"
     vpc_security_group_ids = ["sg-0a13f9496e8f730c7"]
     iam_instance_profile   = aws_iam_instance_profile.main.name
-    depends_on = [aws_iam_instance_profile.main]
     tags = {
         Name = "prometheus-server"
     }
